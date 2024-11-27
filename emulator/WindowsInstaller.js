5f979761c6a67b23da9f49aba170e009eab687cc
Kirk.Sayre <Kirk.Sayre@walmart.com>
2024-11-27 11:35:56 -0600

    Added initial emulation for Word.Application.

--- emulator/WindowsInstaller.js
+++ emulator/WindowsInstaller.js
@@ -6,6 +6,11 @@ function WindowsInstaller() {
         lib.logIOC("WindowsInstaller", {"url": url}, "The script installed a remote MSI.");
         lib.logUrl('Remote MSI Install', url);
     };
+
+    this.openpackage = function(url) {
+        lib.logIOC("WindowsInstaller", {"url": url}, "The script opened a remote MSI package.");
+        lib.logUrl('Remote MSI Install', url);
+    };
 }
 
 module.exports = lib.proxify(WindowsInstaller, "WindowsInstaller");
