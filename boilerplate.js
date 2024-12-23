a3d4e9d227e44f0207cfb0a2ada33660a5077574
Kirk.Sayre <Kirk.Sayre@walmart.com>
2024-12-23 10:53:32 -0600

    Change fake user agent to typical Chrome user agent.

--- boilerplate.js
+++ boilerplate.js
@@ -588,7 +588,7 @@ __fakeParentElem = __createElement("FakeParentElem");
 
 // Stubbed global navigator object.
 const navigator = {
-    userAgent: 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.2; WOW64; Trident/6.0; .NET4.0E; .NET4.0C; .NET CLR 3.5.30729; .NET CLR 2.0.50727; .NET CLR 3.0.30729; Tablet PC 2.0; InfoPath.3)',
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
     clipboard: {
         writeText : function(txt) {
             logIOC('Clipboard', txt, "The script pasted text into the clipboard.");
